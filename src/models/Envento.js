ba3073a8be46461fc7022bc529a46f91787c9157
Israel Lázaro <israellazaromt@gmail.com>
2023-04-18 14:10:59 -0300

    Adicioando o modelo para a criação dos eventos

---
+++ src/models/Envento.js
@@ -0,0 +1,24 @@
+const sequelize = require('../dataBase/db');
+const {DataTypes} = require('sequelize');
+
+const Evento = sequelize.define('Evento', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nomeEvento:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dataInicio:{
+        type: DataTypes.STRING
+    },
+    descricao:{
+        type: DataTypes.STRING
+    },
+    localizacao:{
+        type: DataTypes.GEOMETRY
+    }
+
+});
